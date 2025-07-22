1314ef0e09956b5d4db91639c78d09f29373aca5
RJ <154748733+Titania2@users.noreply.github.com>
2025-07-22 20:50:51 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.json'],   // ✅ Make sure JSON is treated as a static asset
+  assetsInclude: ['**/*.json'],
   esbuild: {
-    loader: 'json',              // ✅ This line tells Vite to load .json files properly
-    include: /\.json$/           // ✅ Optional: applies loader only to .json files
+    loader: 'json',
+    include: /\.json$/
   }
 });
