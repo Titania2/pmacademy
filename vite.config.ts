d2139a3b977494c59131932c1da38fbf2fa3febd
RJ <154748733+Titania2@users.noreply.github.com>
2025-07-22 21:01:43 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.json'],
-  esbuild: {
-    loader: 'json',
-    include: /\.json$/
-  }
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  // Vite already supports JSON imports. These options just make behavior explicit.
+  json: {
+    namedExports: true,
+    stringify: false,
+  },
 });
